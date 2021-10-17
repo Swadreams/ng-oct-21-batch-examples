a4278c81a7279fcbdde1925be2b2bc2699a02cbe
schute <schute@mobiquityinc.com>
2021-10-17 11:57:44 +0530

    *ngFor exmaple

--- ng-examples/src/app/app.component.ts
+++ ng-examples/src/app/app.component.ts
@@ -3,8 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'ng-examples';
+  showMe = false;
+
+  skills = ['html', 'css', 'javascript'];
 }
